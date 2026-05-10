c9dc455b527fb01f2356c0676c0a3b34c16a5a7d
zhnmsadyk <254359@astanait.edu.kz>
2026-05-10 23:41:19 +0500

    Week 3: implemented rental and return logic

--- file_handler.py
+++ file_handler.py
@@ -23,6 +23,7 @@ def _default_vehicles():
         Vehicle(3, "Hyundai", "Sonata",  2021, 55),
         Vehicle(4, "Tesla",   "Model 3", 2023, 95),
         Vehicle(5, "Kia",     "Sportage",2022, 60),
+        Vehicle(6, "Mercedes", "E-Class", 2022, 110),git add .
     ]
 
 def save_history(entry):
